8b8a02056b12d9a40daa033ecf0fb942ac9069f0
おかりな_69B <lcttt.pc@gmail.com>
2025-11-08 23:07:09 +0900

    余計なところ削除（間違えて進めちゃった）

--- app/controllers/daily_notes_controller.rb
+++ app/controllers/daily_notes_controller.rb
@@ -1,6 +1,5 @@
 class DailyNotesController < ApplicationController
   before_action :require_login
-  before_action :set_daily_note, only: [ :edit, :update, :destroy ]
 
   def index
     @daily_notes = current_user.daily_notes.order(date: :desc)
@@ -20,37 +19,17 @@ class DailyNotesController < ApplicationController
     end
   end
 
-  def edit; end
-
-  def update
-    if @daily_note.update(daily_note_params)
-      redirect_to dashboard_path, notice: "日記を更新しました"
-    else
-      flash.now[:alert] = "日記の更新に失敗しました"
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @daily_note.destroy
-    redirect_to dashboard_path, notice: "日記を削除しました"
-  end
-
   private
 
-  def set_daily_note
-    @daily_note = current_user.daily_notes.find(params[:id])
+  def daily_note_params
+    params.require(:daily_note).permit(
+      :condition_score,
+      :mood_score,
+      :did_today,
+      :try_tomorrow,
+      :challenge,
+      :good_things,
+      :memo
+    ).merge(date: Date.current)
   end
-
-def daily_note_params
-  params.require(:daily_note).permit(
-    :condition_score,
-    :mood_score,
-    :did_today,
-    :try_tomorrow,
-    :challenge,
-    :good_things,
-    :memo
-  ).merge(date: Date.current)
-end
 end

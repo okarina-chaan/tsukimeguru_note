184887da18ffa847e9a84d3fe4a285829053fc85
おかりな_69B <lcttt.pc@gmail.com>
2025-11-17 22:35:34 +0900

    リント修正

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -2,10 +2,9 @@ module NavigationHelper
   def main_nav_items
     [
       { name: "Daily", path: daily_notes_path, icon: "pencil-square" },
-      { name: "Moon", path: moon_notes_path, icon: "moon" },
+      { name: "Moon", path: moon_notes_path, icon: "moon" }
       # { name: "分析", path: analytics_path, icon: "chart-bar" },
       # { name: "設定", path: settings_path, icon: "cog-6-tooth" }
     ]
   end
 end
-

ab6cb54f32ad012fce4db67f4ffb4d1662ecf345
おかりな_69B <lcttt.pc@gmail.com>
2025-12-23 16:39:29 +0900

    振り返りを作成するボタンを作成した（168時間に1回使用可能）

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,10 @@ class User < ApplicationRecord
   def account_registered?
     name.present?
   end
+
+  def weekly_insight_available?
+    return true if weekly_insight_generated_at.nil?
+
+    weekly_insight_generated_at < 7.days.ago
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_11_140129) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_23_014827) do
   create_table "daily_notes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.date "date", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_11_140129) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "moon_sign"
+    t.datetime "weekly_insight_generated_at"
     t.index ["line_user_id"], name: "index_users_on_line_user_id", unique: true
   end
 

c721f9b4536da9e2db5d926e69102c9331520ea0
おかりな_69B <lcttt.pc@gmail.com>
2025-12-29 22:23:45 +0900

    viewに渡すデータに揃えた

--- app/controllers/api/weekly_insights_controller.rb
+++ app/controllers/api/weekly_insights_controller.rb
@@ -5,25 +5,26 @@ class Api::WeeklyInsightsController < ApplicationController
 
   def create
     week_key = weekly_insight_week_key(current_user)
-    cached_weekly_insight = Rails.cache.read(week_key)
-    if cached_weekly_insight.present?
+    cached = Rails.cache.read(week_key)
+
+    
+    if cached.present?
       render json: {id: week_key}, status: :ok
       return
-    else
-      weekly_insight = Rails.cache.fetch(week_key, expires_in: 8.days) do
-        reflection = Reflection::MockService.new(daily_notes: fetch_weekly_notes(current_user)).call
-
-        html = render_to_string(
-          partial: "analysis/weekly_insight",
-          locals: { weekly_insight: reflection },
-          layout: false
-        )
-        { id: week_key, html: html }
-      end
-      render json: {id: week_key}, status: :created
     end
-  end
 
+    reflection = Reflection::MockService
+    .new(daily_notes: fetch_weekly_notes(current_user))
+    .call
+
+    Rails.cache.write(
+      week_key,
+      reflection,
+      expires_in: 8.days
+    )
+
+    render json: { id: week_key }, status: :created
+  end
 
   def fragment
     week_key = params[:id]

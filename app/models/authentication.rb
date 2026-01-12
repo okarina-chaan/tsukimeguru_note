b81bc606662540bcf3fb01271f76b12ae2bad884
おかりな_69B <lcttt.pc@gmail.com>
2026-01-12 23:28:04 +0900

    authenticationを追加し

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,3 +1,13 @@
 class Authentication < ApplicationRecord
   belongs_to :user
+
+  # emailでの認証のときのみ使う
+  has_secure_password validations: false
+
+  validates :provider, presence: true, inclusion: { in: %w[line email] }
+  validates :uid, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, if: -> { provider == "email" }
+
+  # providerとuidの組み合わせは一意
+  validates :uid, uniqueness: { scope: :provider }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :daily_notes, dependent: :destroy
   has_many :moon_notes, dependent: :destroy
+  has_many :authentications, dependent: :destroy
   validates :line_user_id, presence: true, uniqueness: true
   validates :account_registered, inclusion: { in: [ true, false ] }
 

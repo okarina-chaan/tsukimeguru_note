1c374e9089e68334c8f3925257210892ce17dab7
おかりな_69B <lcttt.pc@gmail.com>
2026-01-13 23:05:56 +0900

    emailの登録について

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+  def new
+    # ログインフォーム表示
+  end
+
+  def create
+    # email/passwordでのログイン
+    authentication = Authentication.find_by(provider: 'email', uid: params[:email])
+
+    if authentication&.authenticate(params[:password])
+      session[:user_id] = authentication.user_id
+      redirect_to dashboard_path, notice: "ログインしました"
+    else
+      flash.now[:alert] = "メールアドレスまたはパスワードが正しくありません"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     reset_session
     redirect_to root_path, notice: "ログアウトしました"

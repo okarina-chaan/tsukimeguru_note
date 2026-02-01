fc921b4fa396374fd5831cd141dc25e12806e93b
おかりな_69B <lcttt.pc@gmail.com>
2026-02-01 20:37:23 +0900

    未来の日付は選べないようにバリデーションを追加し、成形した

--- app/models/daily_note.rb
+++ app/models/daily_note.rb
@@ -3,18 +3,21 @@ class DailyNote < ApplicationRecord
 
   belongs_to :user
 
-  validates :date, presence: true
+  validates :date,            presence: true
   validates :condition_score, presence: true
-  validates :mood_score, presence: true
+  validates :mood_score,      presence: true
 
   validates :condition_score, inclusion: { in: 1..5, message: "は1〜5の範囲で選択してください" }
-  validates :mood_score, inclusion: { in: 1..5, message: "は1〜5の範囲で選択してください" }
+  validates :mood_score,      inclusion: { in: 1..5, message: "は1〜5の範囲で選択してください" }
 
-  validates :good_things, length: { maximum: 200, message: "は200文字以内で入力してください" }, allow_blank: true
-  validates :try_tomorrow, length: { maximum: 200 }, allow_blank: true
-  validates :did_today, length: { maximum: 200 }, allow_blank: true
-  validates :challenge, length: { maximum: 200 }, allow_blank: true
-  validates :memo, length: { maximum: 1000 }, allow_blank: true
+  validates :good_things,  length: { maximum: 200, message: "は200文字以内で入力してください" }, allow_blank: true
+  validates :try_tomorrow, length: { maximum: 200 },                               allow_blank: true
+  validates :did_today,    length: { maximum: 200 },                               allow_blank: true
+  validates :challenge,    length: { maximum: 200 },                               allow_blank: true
+  validates :memo,         length: { maximum: 1000 },                              allow_blank: true
 
   validates :date, uniqueness: { scope: :user_id, message: "はすでに記入済みです" }
+  validates :date, comparison: {
+    less_than_or_equal_to: -> { Time.zone.today },
+    message: "未来の日付は登録できません" }
 end

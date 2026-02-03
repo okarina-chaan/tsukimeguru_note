b9fa485576a192019cb4657ab367d38ca2c13e09
おかりな_69B <lcttt.pc@gmail.com>
2026-02-03 18:25:21 +0900

    パスワードリセットのコントローラを作成した

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,65 @@
+class PasswordResetsController < ApplicationController
+  def new
+    @email = params[:email]
+  end
+
+  def create
+    if params[:email].blank?
+      redirect_to root_path, alert: "メールアドレスを入力してください"
+      return
+    end
+
+    @email = params[:email]
+    @user = User.find_by(email: params[:email])
+
+    # ユーザーが存在しない場合でも、セキュリティ上の理由から同じメッセージを表示する
+    unless @user
+      redirect_to sent_password_resets_path, notice: "メールを送信しました"
+      return
+    end
+
+    token = @user.signed_id(expires_in: 30.minutes, purpose: :password_reset)
+    reset_url = edit_password_resets_path(token: token)
+    PasswordResetMailer.password_reset_email(@email, reset_url).deliver_now
+
+    redirect_to sent_password_resets_path, notice: "メールを送信しました。"
+  end
+
+  def sent
+  end
+
+  def edit
+  end
+
+  def update
+    @user = User.find_signed(params[:token], purpose: :password_reset)
+
+    unless @user
+      redirect_to root_path, alert: "このリンクは無効か、有効期限が切れています"
+      return
+    end
+
+    if params[:password].blank?
+      flash.now[:alert] = "パスワードを入力してください"
+      render :edit, status: :unprocessable_entity
+      return
+    end
+
+    if params[:password] != params[:password_confirmation]
+      flash.now[:alert] = "パスワードが一致しません"
+      render :edit, status: :unprocessable_entity
+      return
+    end
+    email_auth = @user.email_authentication
+
+    if email_auth
+      email_auth.update(
+        password: params[:password],
+        password_confirmation: params[:password_confirmation]
+      )
+      redirect_to new_session_path, notice: "パスワードをリセットしました。ログインしてください"
+    else
+      redirect_to root_path, alert: "メール認証が設定されていません"
+    end
+  end
+end

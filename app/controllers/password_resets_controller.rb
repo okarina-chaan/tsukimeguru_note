2ad83c6866f70356dc7cf639bd7f18114676ad39
おかりな_69B <lcttt.pc@gmail.com>
2026-02-04 10:47:53 +0900

    rubocopに怒られたのでメッセージをi18n化した

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,7 +10,7 @@ class PasswordResetsController < ApplicationController
 
   def create
     if params[:email].blank?
-      redirect_to root_path, alert: "メールアドレスを入力してください"
+      redirect_to root_path, alert: t(".email_blank")
       return
     end
 
@@ -19,7 +19,7 @@ class PasswordResetsController < ApplicationController
 
     # ユーザーが存在しない場合でも、セキュリティ上の理由から同じメッセージを表示する
     unless @user
-      redirect_to sent_password_resets_path, notice: "メールを送信しました"
+      redirect_to sent_password_resets_path, notice: t(".email_sent")
       return
     end
 
@@ -27,7 +27,7 @@ class PasswordResetsController < ApplicationController
     reset_url = edit_password_resets_url(token: token)
     PasswordResetMailer.password_reset_email(@email, reset_url).deliver_now
 
-    redirect_to sent_password_resets_path, notice: "メールを送信しました。"
+    redirect_to sent_password_resets_path, notice: t(".email_sent_with_period")
   end
 
   def sent
@@ -36,30 +36,28 @@ class PasswordResetsController < ApplicationController
   def edit
     @user = User.find_signed(params[:token], purpose: :password_reset)
 
-    # rubocop:disable Rails/I18nLocaleTexts
     unless @user
-      redirect_to root_path, alert: "このリンクは無効か、有効期限が切れています"
+      redirect_to root_path, alert: t(".invalid_or_expired_link")
       nil
     end
-    # rubocop:enable Rails/I18nLocaleTexts
   end
 
   def update
     @user = User.find_signed(params[:token], purpose: :password_reset)
 
     unless @user
-      redirect_to root_path, alert: "このリンクは無効か、有効期限が切れています"
+      redirect_to root_path, alert: t(".invalid_or_expired_link")
       return
     end
 
     if params[:password].blank?
-      flash.now[:alert] = "パスワードを入力してください"
+      flash.now[:alert] = t(".password_blank")
       render :edit, status: :unprocessable_entity
       return
     end
 
     if params[:password] != params[:password_confirmation]
-      flash.now[:alert] = "パスワードが一致しません"
+      flash.now[:alert] = t(".password_mismatch")
       render :edit, status: :unprocessable_entity
       return
     end
@@ -70,9 +68,9 @@ class PasswordResetsController < ApplicationController
         password: params[:password],
         password_confirmation: params[:password_confirmation]
       )
-      redirect_to new_session_path, notice: "パスワードをリセットしました。ログインしてください"
+      redirect_to new_session_path, notice: t(".password_reset_success")
     else
-      redirect_to root_path, alert: "メール認証が設定されていません"
+      redirect_to root_path, alert: t(".email_auth_not_set")
     end
   end
 end

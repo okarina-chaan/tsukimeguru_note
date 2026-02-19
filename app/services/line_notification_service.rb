d12e0b224c74788a7a8a21abf2cc2750dfd54ea9
おかりな_69B <lcttt.pc@gmail.com>
2026-02-19 22:25:32 +0900

    LINE messageをV2の書き方に直して、それにあわせてテストも書き直した

--- app/services/line_notification_service.rb
+++ app/services/line_notification_service.rb
@@ -2,31 +2,38 @@ require "line/bot"
 
 class LineNotificationService
   def self.client
-    Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_MESSAGE_CHANNEL_SECRET"]
-      config.channel_token  = ENV["LINE_MESSAGE_CHANNEL_ID"]
-    }
+    @client ||= Line::Bot::V2::MessagingApi::ApiClient.new(
+      channel_access_token: ENV["LINE_MESSAGING_API_CHANNEL_ACCESS_TOKEN"]
+    )
   end
 
   def self.notify(user, message)
     return unless user.line_user_id.present?
 
-    client.push_message(user.line_user_id, {
-      type: "text",
-      text: message
-    })
+    push_request = Line::Bot::V2::MessagingApi::PushMessageRequest.new(
+      to: user.line_user_id,
+      messages: [
+        Line::Bot::V2::MessagingApi::TextMessage.new(
+          type: "text",
+          text: message
+        )
+      ]
+    )
+    client.push_message(push_request)
   end
+end
 
+class MoonNoteMessageService
   def self.message_for(phase)
     case phase
     when :new_moon
-      "今日は新月のMoon Noteの日です。あなたの目標を宣言しましょう。#{ENV['APP_URL']}"
+      "今日は新月のMoon Noteの日です。\nあなたの目標を宣言しましょう。\n#{ENV['APP_URL']}"
     when :full_moon
-      "今日は満月のMoon Noteの日です。これまで頑張ったこと・達成したことを振り返りましょう。#{ENV['APP_URL']}"
+      "今日は満月のMoon Noteの日です。\nこれまで頑張ったこと・達成したことを振り返りましょう。\n#{ENV['APP_URL']}"
     when :first_quarter_moon
-      "今日は上弦の月のMoon Noteの日です。あなたの目標に向けて、吸収したいことや挑戦したいことを考えてみましょう！#{ENV['APP_URL']}"
+      "今日は上弦の月のMoon Noteの日です。\nあなたの目標に向けて、吸収したいことや挑戦したいことを考えてみましょう！\n#{ENV['APP_URL']}"
     when :last_quarter_moon
-      "今日は下弦の月のMoon Noteの日です。次の目標のために、手放したいことを考えてみましょう！#{ENV['APP_URL']}"
+      "今日は下弦の月のMoon Noteの日です。\n次の目標のために、手放したいことを考えてみましょう！\n#{ENV['APP_URL']}"
     else
       nil
     end
--- spec/services/line_notification_service_spec.rb
+++ spec/services/line_notification_service_spec.rb
@@ -4,23 +4,23 @@ require "line/bot"
 RSpec.describe LineNotificationService do
     describe ".message_for" do
     it "新月のときは新月のMoon Noteの日のメッセージが返る" do
-      expect(LineNotificationService.message_for(:new_moon)).to include("新月のMoon Noteの日")
+      expect(MoonNoteMessageService.message_for(:new_moon)).to include("新月のMoon Noteの日")
     end
 
     it "満月のときは満月のMoon Noteの日のメッセージが返る" do
-      expect(LineNotificationService.message_for(:full_moon)).to include("満月のMoon Noteの日")
+      expect(MoonNoteMessageService.message_for(:full_moon)).to include("満月のMoon Noteの日")
     end
 
     it "上弦の月のときは上弦の月のMoon Noteの日のメッセージが返る" do
-      expect(LineNotificationService.message_for(:first_quarter_moon)).to include("上弦の月のMoon Noteの日")
+      expect(MoonNoteMessageService.message_for(:first_quarter_moon)).to include("上弦の月のMoon Noteの日")
     end
 
     it "下弦の月のときは下弦の月のMoon Noteの日のメッセージが返る" do
-      expect(LineNotificationService.message_for(:last_quarter_moon)).to include("下弦の月のMoon Noteの日")
+      expect(MoonNoteMessageService.message_for(:last_quarter_moon)).to include("下弦の月のMoon Noteの日")
     end
 
     it "不明なフェーズのときはnilが返る" do
-      expect(LineNotificationService.message_for(:unknown_phase)).to be_nil
+      expect(MoonNoteMessageService.message_for(:unknown_phase)).to be_nil
     end
   end
 
@@ -35,7 +35,7 @@ RSpec.describe LineNotificationService do
     it "line_user_idがあるユーザーにpush_messageが呼ばれる" do
       user = create(:user, line_user_id: "LINE123")
       LineNotificationService.notify(user, "テストメッセージ")
-      expect(mock_client).to have_received(:push_message).with("LINE123", anything)
+      expect(mock_client).to have_received(:push_message).with(instance_of(Line::Bot::V2::MessagingApi::PushMessageRequest))
     end
 
     it "line_user_idがないユーザーにはpush_messageが呼ばれない" do
@@ -44,5 +44,4 @@ RSpec.describe LineNotificationService do
       expect(mock_client).not_to have_received(:push_message)
     end
   end
-
 end

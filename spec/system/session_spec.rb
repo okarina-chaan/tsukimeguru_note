716cbad9507c025dd9d765e10c6ef5cc557c03fc
おかりな_69B <lcttt.pc@gmail.com>
2026-03-08 15:32:47 +0900

    capybaraのやり方でsleepじゃなくてwaitを使うように変えた

--- spec/system/session_spec.rb
+++ spec/system/session_spec.rb
@@ -18,10 +18,7 @@ RSpec.describe "セッション管理", type: :system do
     click_link "ログイン", match: :first
     find("a[href='/line_login_api/login']", visible: :all, match: :first).click
 
-    sleep 3
-
-    expect(page).to have_content("今日はどんな記録を残しますか？")
-    expect(page).to have_current_path(dashboard_path)
+    expect(page).to have_current_path(dashboard_path, wait: 10)
 
     click_on "ログアウト"
     expect(page).to have_current_path(root_path)
